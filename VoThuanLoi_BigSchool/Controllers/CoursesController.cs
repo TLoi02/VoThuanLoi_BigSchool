72cec771aa5c9ec6d11b0de69a867377d01ae0e9
Vothuanloi123 <vothuanloi110802@gmail.com>
2023-03-08 11:04:02 +0700

    Add project files.

---
+++ VoThuanLoi_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VoThuanLoi_BigSchool.Models;
+using VoThuanLoi_BigSchool.ViewModels;
+
+namespace VoThuanLoi_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
